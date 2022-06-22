0a79a054effd5e5d044496749b4cee145abf8e07
Andres Uris <andres.urismontilla@gmail.com>
2022-06-22 01:56:29 -0400

    refactor: storage controller

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, HttpStatus, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from './storage.config';
 import { StorageService } from './storage.service';
@@ -9,14 +16,16 @@ export class StorageController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file', multerOptions))
-  async upload(@UploadedFile() file: Express.Multer.File, @Res() response) {
+  async upload(@UploadedFile() file: Express.Multer.File, @Res() res) {
     try {
       await this.storageService.upload(file);
-      return response.status(HttpStatus.OK).send('File uploaded successfully');
-    }
-    catch(error) {
-      console.log(error)
-      return response.status(HttpStatus.BAD_GATEWAY).send('Storage service unavailable');
+      return res.status(HttpStatus.OK).send('File uploaded successfully');
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(HttpStatus.BAD_GATEWAY)
+        .send('Storage service unavailable');
     }
   }
 }
